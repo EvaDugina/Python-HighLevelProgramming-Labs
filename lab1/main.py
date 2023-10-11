cf4fd9a409502663f26f0ad7f804ce08b2a7d9cf
JohnDugin <vania.dugin917@mail.ru>
2023-10-11 03:53:14 +0300

    lab1

---
+++ lab1/main.py
@@ -0,0 +1,206 @@
+import time
+
+
+# 1. Передача параметров в функции происходит через присваивание (pass-by-assignment)
+# те. в момент вызова ф-ции параметр как бы связывается с объектом из сигнатуры ф-ции и дальше уже всё зависит
+# от типа, точнее от того, является этот тип изменяемым или неизменяемым. То есть при передаче изменяемого в ф-цию
+# при изменении этого объекта в функции изменится и он сам, в то время как передавая int, str и тд. даже при изменении
+# создастся новый объект. А при присваивании в любом случае создастся новый объект внутри функции.
+# Попытался так сделать в ф-ции myLensort, но функция sorted по-видимому не изменяет массив, а где-то у себя внутри
+# совершает присваивание, да это и логично. Не правда ли? И правда.
+# << функция sorted возвращает новый отсортированный список, который получен из итерируемого объекта,
+# который был передан как аргумент >>
+
+# 2. Ну и собственно. Как будто в предыдущем ответе я к этому как раз и подвёл.
+# sort() сортирует список на месте, изменяя его индексы и возвращая None, тогда как sorted() -> см. предыдущий ответ
+# Ну это уже из сигнатуры видно. Что один array.sort(...), а второй sorted(array, ...)
+
+# 3. Ну. Лихо. Лихо их использовать надо
+
+def my_lensorted(array) -> []:
+    return sorted(array, key=lambda element: len(element))
+
+
+def my_lensort(array):
+    array.sort(key=lambda element: len(element))
+
+
+def task_1():
+    print("Результат Задания №1:")
+    array = ['python', 'perl', 'java', 'c', 'haskel', 'ruby']
+    print(my_lensorted(array))
+    my_lensort(array)
+    print(array)
+    print()
+
+
+#
+#
+#
+
+# 1. Set представляет из себя множество состоящее из неповторяющихся элементов неизменяемых типов!
+# Те. неупорядоченная коллекция уникальных элементов иммутабельных типов, основанная на ХЭШ-ТАБЛИЦАХ, вот так!
+# Ну, это всё так, знаете ли. Ежели по-научному размовлять.
+# Ну и кроме этого. Если говорить про скорость поиска элементов, то вот кое-что об этом:
+# Данный метод работает так: каждый элемент присваивается какому-то классу элементов (например, класс элементов,
+# имеющих одинаковый остаток от деления на модуль). Все элементы каждого класса хранятся в отдельном списке.
+# В таком случае мы заранее знаем, в каком списке должен находиться элемент,
+# и можем за короткое время выполнить необходимые операции.
+# Дубликаты он сам удаляет. То есть просто не добавляет
+
+def my_unique(array) -> []:
+    return set(array)
+
+
+def task_2():
+    print("Результат Задания №2:")
+    array = [1, 2, 1, 3, 2, 5]
+    print(my_unique(array))
+    print()
+
+
+#
+#
+#
+
+# 1. range([start], stop[, step]) - генерирует список чисел
+# 2. функция zip позволяет пройтись одновременно по нескольким итерируемым объектам:
+# zip(a, b) создает объект-итератор, из которого при каждом обороте цикла извлекается кортеж,
+# состоящий из двух элементов
+
+ListOfTuples = list[tuple]
+
+
+def my_enumerate(array) -> ListOfTuples:
+    list_tuples = list()
+    for i in range(0, len(array)):
+        tuple = (i, array[i])
+        list_tuples.append(tuple)
+    return list_tuples
+
+
+def my_enumerate_with_zip(array):
+    array_numbers = list(range(0, len(array)))
+    list_tuples = list(zip(array_numbers, array))
+    return list_tuples
+
+
+def task_3():
+    print("Результат Задания №3:")
+    array = ['a', 'b', 'c']
+    # array_1 = my_enumerate(array)
+    # print(array_1)
+    array_2 = my_enumerate_with_zip(array)
+    print(array_2)
+    print()
+
+
+#
+#
+#
+
+# 1. dict - неупорядоченные коллекции произвольных объектов с доступом по ключу
+# 3. Функция split сканирует всю строку и разделяет ее в случае нахождения разделителя. Иначе помещает всё в массив
+# под нулевым индексом
+def task_4(filename):
+    print("Результат Задания №4:")
+    # print("sdkv;ansvn".split())
+    dict_count_words = dict()
+    with open(filename, "r", encoding="utf-8") as file:
+        text = file.read()
+        words = text.replace("\n", " ").split()
+        for word in words:
+            dict_count_words.setdefault(word, 0)
+            dict_count_words[word] += 1
+            # if word not in return_dict.keys():
+            # return_dict[word] = 1
+            # else:
+            #     return_dict[word] += 1
+    print(dict_count_words)
+    print(dict(sorted(dict_count_words.items(), key=lambda element: element[1], reverse=True)))
+    print()
+
+
+#
+#
+#
+
+
+def time_decorator(func):
+    def wrapper(*args, **kwargs):
+        print("Вызов функции: ", func.__name__)
+
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+
+        # print(result)
+
+        elapsed_time = end_time - start_time
+        print('Время выполнения ф-ции: ', elapsed_time)
+        print()
+
+        return result
+
+    return wrapper
+
+
+@time_decorator
+def square_for(list_nums) -> list:
+    list_square = list()
+    for num in list_nums:
+        list_square.append(num**2)
+    return list_square
+
+@time_decorator
+def square_list(list_nums) -> list:
+    return [num**2 for num in list_nums]
+
+@time_decorator
+def square_map(list_nums) -> list:
+    return list(map(lambda x: x**2, list_nums))
+
+def square_variations(count):
+    list_nums = range(count)
+    print("Количество значений: ", count)
+    print()
+
+    square_for(list_nums)
+    square_list(list_nums)
+    square_map(list_nums)
+
+
+def task_5():
+    print("Результат Задания №5:")
+    print()
+
+    square_variations(10000)
+    # 0.0019979476928710938
+    # 0.0019996166229248047
+    # 0.0020003318786621094
+
+    # square_variations(10000000)
+    # 3.1948530673980713
+    # 2.7440342903137207
+    # 3.207818031311035
+
+    # square_variations(100000000)
+    # 34.24076008796692
+    # 27.57021975517273
+    # 32.81210684776306
+
+
+
+
+#
+#
+#
+
+if __name__ == '__main__':
+    # task_1()
+    # task_2()
+    # task_3()
+    # task_4("task4.txt")
+    # task_5()
+    # task_6()
+    print("THE HAPPY END!")
---
+++ lab1/storage.py
@@ -0,0 +1,84 @@
+import argparse
+import json
+
+# storage.py --key key_name --val value
+# storage.py --key key_name
+
+# --key <имя ключа>, где <имя ключа> - ключ по которому сохраняются/получаются значения
+# --val <значение>, где <значение> - сохраняемое значение.
+
+# ВЫВОД ПО КЛЮЧУ: value_1, value_2 или None
+import os
+
+FILE_NAME = "storage.json"
+
+
+def work(key=None, value=None):
+    if key is None:
+        print("Invalid arguments!")
+
+    if value is None:
+        print(get_value(key))
+    else:
+        set_value(key, value)
+
+
+def get_value(key):
+    text = get_file_contents()
+    if text:
+        data = json.loads(text)
+        index = find_key(key, data)
+        if index != -1:
+            return ", ".join(data[index].get(key))
+        else:
+            return "None"
+    else:
+        return "None"
+
+
+def set_value(key, value):
+    text = get_file_contents()
+    if text:
+        data = json.loads(text)
+        index = find_key(key, data)
+        if index != -1:
+            data[index].get(key).append(value)
+        else:
+            data.append({key: [value]})
+        new_text = json.dumps(data)
+    else:
+        new_text = json.dumps([{key: [value]}])
+
+    put_file_contents(new_text)
+
+
+def find_key(key, data):
+    for index, element in enumerate(data):
+        if element.get(key) is not None:
+            return index
+    return -1
+
+
+def get_file_contents():
+    with open(FILE_NAME, "r", encoding="utf-8") as file:
+        text = file.read()
+        file.close()
+    return text
+
+def put_file_contents(text):
+    with open(FILE_NAME, "w", encoding="utf-8") as file:
+        file.write(text)
+        file.close()
+
+
+parser = argparse.ArgumentParser(description="Task №6", exit_on_error=False)
+parser.add_argument('--key', dest="key", type=str)
+parser.add_argument('--val', dest="value", type=str)
+
+try:
+    args = parser.parse_args()
+    work(args.key, args.value)
+except argparse.ArgumentError:
+    print("Invalid arguments!")
+
+

43595052b90124880c78623ff6774817be4402e7
EvaDugina <vania.dugin@bk.ru>
2024-01-06 20:13:33 +0300

    fix

--- lab2/Decorators.py
+++ lab2/Decorators.py
@@ -9,13 +9,19 @@ class BaseDecorator:
 
     def __call__(self, *args, **kwargs):
         self.decorate(*args, **kwargs)
+        self.log_info(*args)
+        print()
+
+    @property
+    def __name__(self):
+        return self._function.__name__
 
     def decorate(self, *args, **kwargs):
         pass
 
     def log_info(self, *args):
         now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
+        current_time = now.strftime("%H:%M:%S.%f")
         self.list_info.append(f"{current_time}: function {self._function.__name__} called with arguments {args}")
         print(self.list_info)
 
@@ -30,8 +36,6 @@ class TimerDecorator(BaseDecorator):
         start_time = time.perf_counter()
         result = self._function(*args, **kwargs)
         self.runtime = time.perf_counter() - start_time
-
-        self.log_info(*args)
 
         return result
 

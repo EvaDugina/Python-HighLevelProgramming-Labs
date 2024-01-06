3aedb3cbe8319cdd7bca2b57a33c939f1e552825
EvaDugina <vania.dugin@bk.ru>
2024-01-06 18:33:44 +0300

    fix: Decorators

--- lab2/Decorators.py
+++ lab2/Decorators.py
@@ -3,30 +3,15 @@ from datetime import datetime
 
 
 class BaseDecorator:
-
     def __init__(self, my_function):
         self._function = my_function
-        self._info = list()
+        self.list_info = list()
 
-    def __call__(self, *args, **kwargs):
-        self.decorate()
-        result = self._function(*args, **kwargs)
-        self.keep_history()
-        self.log_info(*args)
-        return result
-
-    def log_info(self, *args):
+    def _logInfo(self, *args):
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
-        self._info.append(f"{current_time}: function {self._function.__name__} called with arguments {args}")
-        print(self._info)
-
-    def decorate(self):
-        pass
-
-    def keep_history(self):
-        pass
-
+        self.list_info.append(f"{current_time}: function {self._function.__name__} called with arguments {args}")
+        print(self.list_info)
 
 
 class TimerDecorator(BaseDecorator):
@@ -35,18 +20,18 @@ class TimerDecorator(BaseDecorator):
     def __name__(self):
         return self._function.__name__
 
-    def decorate(self):
+    def __call__(self, *args, **kwargs):
         self.start_time = time.perf_counter()
-
-    def keep_history(self):
+        result = self._function(*args, **kwargs)
         self.runtime = time.perf_counter() - self.start_time
-
+        self._logInfo(*args)
+        return result
 
 
 class HtmlDecorator(BaseDecorator):
 
-    def decorate(self):
-        pass
-
-    def keep_history(self):
+    def __call__(self, *args, **kwargs):
+        result = self._function(*args, **kwargs)
+        self._logInfo(*args)
         print(f"<html><body>{self._function.runtime:.10f}</body></html>")
+        return result
